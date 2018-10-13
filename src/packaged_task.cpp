5f04e5f8c373f489906840bc41b2067c7a5217e7
Jun Chen <junc76@gmail.com>
2018-10-14 07:36:40 +0800

    packaged_task: push task in a queue

--- src/packaged_task.cpp
+++ src/packaged_task.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <deque>
 #include <future>
 #include <gtest/gtest.h>
 #include <iostream>
@@ -34,6 +35,54 @@ TEST(packaged_task, simple)
 
     std::cout << "The countdown lasted for " << value << " seconds.\n";
     th.join();
+}
+
+TEST(packaged_task, simple_1)
+{
+    std::packaged_task<int()> task(std::bind(countdown, 10, 0));
+    std::future<int> ret = task.get_future();
+    std::thread th(std::move(task));
+    int value = ret.get();
+
+    std::cout << "The countdown lasted for " << value << " seconds.\n";
+    th.join();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// No. 2
+// Title: C++ Threading #9: packaged_task
+// Web: https://www.youtube.com/watch?v=FfbZfBk-3rI&index=9&list=PL5jc9xFGsL8E12so1wlMS0r0hTQoJL74M
+///////////////////////////////////////////////////////////////////////////////
+
+std::deque<std::packaged_task<int()>> task_q;
+std::mutex mu;
+std::condition_variable cv;
+
+void work()
+{
+    std::packaged_task<int()> t;
+    {
+        std::unique_lock<std::mutex> locker(mu);
+        cv.wait(locker, []() { return !task_q.empty(); });
+        t = std::move(task_q.front());
+        task_q.pop_front();
+    }
+    t();
+}
+
+TEST(packaged_task, queue)
+{
+    std::thread workthread(work);
+    std::packaged_task<int()> task(std::bind(countdown, 10, 0));
+    std::future<int> fu = task.get_future();
+    {
+        std::lock_guard<std::mutex> locker(mu);
+        task_q.push_back(std::move(task));
+    }
+    cv.notify_one();
+    std::cout << fu.get() << '\n';
+
+    workthread.join();
 }
 
 } // namespace packaged_task
