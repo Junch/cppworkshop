f7b635e228ef86726005978bb176c26565115a9b
Jun Chen <junc76@gmail.com>
2018-10-26 11:40:41 +0800

    update build.sh to distinguish between building code coverage version and building standard version

--- build.sh
+++ build.sh
@@ -1,8 +1,50 @@
-mkdir _build > /dev/null
-pushd _build > /dev/null
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dcoverage=ON ..
-make VERBOSE=1
-make test
-ctest
-make Test_coverage
+#!/bin/bash
+
+HELP=false
+VERBOSE=false
+COVERAGE=OFF
+
+while getopts vhc opt
+do
+    case ${opt}
+    in
+        v) VERBOSE=true ;;
+        h) HELP=true    ;;
+        c) COVERAGE=ON ;;
+    esac
+done
+
+BUILDDIR=_build
+if [ ${COVERAGE} = ON ]; then
+    BUILDDIR=_build_coverage
+fi
+
+echo VERBOSE=${VERBOSE}
+echo HELP=${HELP}
+echo COVERAGE=${COVERAGE}
+echo BUILDDIR=${BUILDDIR}
+
+if [ ${HELP} = true ]; then
+    echo "-v Output verbose information in making"
+    echo "-h Display help information"
+    echo "-c Build and run the version to get the code coverage"
+    exit 1
+fi
+
+mkdir ${BUILDDIR} > /dev/null
+pushd ${BUILDDIR} > /dev/null
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -Dcoverage=${COVERAGE} ..
+
+if [ ${VERBOSE} = true ]; then
+    make VERBOSE=1
+else
+    make
+fi
+
+if [ ${COVERAGE} = ON ]; then
+    make test
+    ctest
+    make Test_coverage
+fi
+
 popd > /dev/null
