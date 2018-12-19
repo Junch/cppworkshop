a72963aeba0dd0039809edad1208000d130c6038
Jun Chen <junc76@gmail.com>
2018-12-19 14:57:39 +0800

    Fix the link issue on windows for the glog library

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @mkdir _build >nul
 pushd _build >nul
 cmake ..
-cmake --build .
+cmake --build . --config Debug -- /verbosity:detailed
+REM cmake --build . --config Release -- /verbosity:detailed
 popd >nul
