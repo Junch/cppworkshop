8142f9be51b266b82e489018ddbc2ddfa26169cc
Jun Chen <junc76@gmail.com>
2019-03-06 15:02:33 +0800

    Add more tests for std::map

--- src/map_test.cpp
+++ src/map_test.cpp
@@ -1,4 +1,6 @@
+#include "type_name.h"
 #include <gtest/gtest.h>
+#include <iostream>
 #include <map>
 #include <string>
 
@@ -24,4 +26,40 @@ TEST(map, emplace)
 
     number[2] = "Deux";
     ASSERT_STREQ("Deux", number[2].c_str());
+}
+
+TEST(map, lower_bound)
+{
+    std::map<int, std::string> number{{1, "One"}, {2, "Two"}, {4, "Four"}, {5, "Five"}};
+    {
+        auto itlow = number.lower_bound(3); // Iterator pointing to the first element that is not less than key.
+        auto itup = number.upper_bound(3);  // Iterator pointing to the first element that is greater than key
+
+        ASSERT_EQ(4, itlow->first);
+        ASSERT_EQ(4, itup->first);
+    }
+
+    {
+        auto itlow = number.lower_bound(4); // Iterator pointing to the first element that is not less than key.
+        auto itup = number.upper_bound(4);  // Iterator pointing to the first element that is greater than key
+
+        ASSERT_EQ(4, itlow->first);
+        ASSERT_EQ(5, itup->first);
+    }
+}
+
+TEST(map, type)
+{
+    std::map<int, std::string> number{{1, "One"}, {2, "Two"}, {3, "Three"}};
+    auto iter1 = number.find(2);
+    std::cout << "decltype(iter1) is " << type_name<decltype(iter1)>() << '\n';
+
+    const auto iter2 = number.find(2);
+    std::cout << "decltype(iter2) is " << type_name<decltype(iter2)>() << '\n';
+
+    std::map<int, std::string>::iterator iter3 = number.find(2);
+    std::cout << "decltype(iter3) is " << type_name<decltype(iter3)>() << '\n';
+
+    std::map<int, std::string>::const_iterator iter4 = number.find(2);
+    std::cout << "decltype(iter4) is " << type_name<decltype(iter4)>() << '\n';
 }
