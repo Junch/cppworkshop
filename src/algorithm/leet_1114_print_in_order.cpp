63c1b4da0c04c526e6cca0f83473887ccd9795e1
Jun Chen <junc76@gmail.com>
2019-08-12 14:15:00 +0800

    Add a new folder focus on algorithm

---
+++ src/algorithm/leet_1114_print_in_order.cpp
@@ -0,0 +1,88 @@
+#include <functional>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <mutex>
+#include <thread>
+using namespace std;
+using namespace std::literals;
+
+///////////////////////////////////////////////////////////////////////////////
+// Title: 1114. Print in Order
+// Web: https://leetcode.com/problems/print-in-order
+///////////////////////////////////////////////////////////////////////////////
+
+void printFirst()
+{
+    std::cout << "first";
+}
+
+void printSecond()
+{
+    std::cout << "second";
+}
+
+void printThird()
+{
+    std::cout << "third";
+}
+
+class Foo
+{
+  public:
+    Foo()
+    {
+        m1.lock();
+        m2.lock();
+    }
+
+    void first(function<void()> printFirst)
+    {
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        m1.unlock();
+    }
+
+    void second(function<void()> printSecond)
+    {
+        m1.lock();
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        m2.unlock();
+    }
+
+    void third(function<void()> printThird)
+    {
+        m2.lock();
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+
+  private:
+    std::mutex m1, m2;
+};
+
+TEST(leet_1114, 123)
+{
+    Foo foo;
+    std::thread t1(&Foo::first, &foo, printFirst);
+    std::this_thread::sleep_for(100ms);
+    std::thread t2(&Foo::second, &foo, printSecond);
+    std::this_thread::sleep_for(100ms);
+    std::thread t3(&Foo::third, &foo, printThird);
+    t1.join();
+    t2.join();
+    t3.join();
+}
+
+TEST(leet_1114, 132)
+{
+    Foo foo;
+    std::thread t1(&Foo::first, &foo, printFirst);
+    std::this_thread::sleep_for(100ms);
+    std::thread t2(&Foo::third, &foo, printThird);
+    std::this_thread::sleep_for(100ms);
+    std::thread t3(&Foo::second, &foo, printSecond);
+    t1.join();
+    t2.join();
+    t3.join();
+}
